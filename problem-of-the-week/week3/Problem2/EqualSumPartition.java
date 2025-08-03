83ddcdf8d885c67b16789c63f05a0b8cd55990e7
Gopeshsharmaa <gopesh.sharma_cs23@gla.ac.in>
2025-08-03 09:54:14 +0530

    Add files via upload

---
+++ problem-of-the-week/week3/Problem2/EqualSumPartition.java
@@ -0,0 +1,35 @@
+public class EqualSumPartition {
+
+    public static boolean canPartition(int[] nums) {
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        // If total sum is odd, we cannot partition it into equal subsets
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {15, 5, 20, 10, 35, 15, 10};
+        System.out.println(canPartition(arr1)); // Output: true
+
+        int[] arr2 = {15, 5, 20, 10, 35};
+        System.out.println(canPartition(arr2)); // Output: false
+    }
+}
